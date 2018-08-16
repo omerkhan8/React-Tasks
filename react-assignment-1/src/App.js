e84aad6616f220da37d965b6c9f86cc59e733e29
Omer Khan <omerkhan842@gmail.com>
2018-08-16 12:35:35 +0500

    complex variables in JSX

--- react-assignment-1/src/App.js
+++ react-assignment-1/src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+
+    const name = "Hello World";
+    const obj = { name: "Hello World Object" }
+    const data = ['We', 'are', 'United'] //Show these in seperate tags
+    const list = [{ name: "Hello World 1" }, { name: "Hello World 2" }, { name: "Hello World 3" }] //Show these in seperate tags
+    const complex = [{ company: 'XYZ', jobs: ['Javascript', 'React'] }, { company: 'ABC', jobs: ['AngularJs', 'Ionic'] }]
+
+
     return (
-      <div class = "nav"><h3>First React Assignment </h3></div>
+      <div id="main">
+        <div id="nav"><h3>First React Assignment </h3></div>
+        <div>
+          <p>{name}
+            <br />
+            {obj.name}
+            <br />
+            {data.map(val => <li key={Math.random().toString().slice(2, 6)} >{val}</li>)}
+            <br />
+          </p>
+          <ol>
+            {list.map(val => <li key={Math.random().toString().slice(2, 6)}>{val.name}</li>)}
+          </ol>
+          <h5>Complex:</h5>
+          <ol>
+            {complex.map(val => <li key={Math.random().toString().slice(2, 6)}>{val.company}
+              <ul>{val.jobs.map(items => <li key={Math.random().toString().slice(1, 6)}>{items}</li>)}</ul></li>)}
+          </ol>
+
+        </div>
+
+      </div>
     );
   }
 }
