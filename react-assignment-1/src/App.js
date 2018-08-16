7a620c1a4db3279680d49318bece9957206ad781
Omer Khan <omerkhan842@gmail.com>
2018-08-16 11:25:34 +0500

    Basic navbar added

--- react-assignment-1/src/App.js
+++ react-assignment-1/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div class = "nav"></div>
+      <div class = "nav"><h3>First React Assignment </h3></div>
     );
   }
 }
