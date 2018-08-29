534fd6125c930db286e35f4e27b42d9891ce558d
Omer Khan <omerkhan842@gmail.com>
2018-08-29 13:42:20 +0500

    updated

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,6 +4,33 @@ import logo1 from './images/logo1.png';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      todoList: [],
+      text: ''
+    }
+
+    this.getText = this.getText.bind(this);
+  }
+
+  getText(e) {
+    let { text } = this.state;
+    text = e.target.value;
+    this.setState({ text })
+
+  }
+
+  addTodo() {
+    let { todoList, text } = this.state;
+    todoList.push(text);
+    text = '';
+    this.setState({ todoList, text });
+    console.log(this.state)
+  }
+
+
+
   renderHeader() {
     return (
       <div className='navBarr'>
@@ -17,10 +44,13 @@ class App extends Component {
     return (
       <div>
         <div className='input'>
-          <input type="text" />
+          <input type="text" onChange={this.getText} />
           <span className="icon11">
-            <i className ="fa fa-plus-square" style={{ fontSize: '40px', color: '#5BB06C', }}></i>
+            <i className="fa fa-plus-square" style={{ fontSize: '40px', color: '#5BB06C', }} onClick={() => this.addTodo()}></i>
           </span>
+        </div>
+        <div className='myList'>
+
         </div>
       </div>
     )
