1016a897c9d6a1a56a8daff5452963f3d7c04688
Omer Khan <omerkhan842@gmail.com>
2018-09-01 21:13:37 +0500

    login page completed

--- react-assignment-3/src/App.js
+++ react-assignment-3/src/App.js
@@ -13,20 +13,23 @@ class App extends Component {
   }
 
   loginPage() {
+
+    document.body.style.backgroundImage = `url(${background})`
     return (
       <div>
         <div className='loginDiv'>
           <h2 className='adminHeading'>Admin Login</h2>
           <div id='line'></div>
           <div className="form-group">
-
             <input type="email" className="form-control" placeholder="Enter email" />
-            <small id="emailHelp" className="form-text" style={{color:'white'}}>Email associated with admin account.</small>
+            <small id="emailHelp" className="form-text" style={{ color: 'white' }}>Email associated with admin account.</small>
           </div>
           <div className="form-group">
             <input type="password" className="form-control" placeholder="Password" />
           </div>
-
+          <div className='loginBtn'>
+            <button type="submit" className="btn btn-dark">Login</button>
+          </div>
         </div>
       </div>
     )
