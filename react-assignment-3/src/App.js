5a8b1913fced9d1795a8a5b84efbf465fd15a506
Omer Khan <omerkhan842@gmail.com>
2018-09-02 18:21:23 +0500

    Login component added

--- react-assignment-3/src/App.js
+++ react-assignment-3/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import swal from 'sweetalert';
-import background from './images/background.jpg'
+import Login from './components/Login/Login'
+// import swal from 'sweetalert';
+// import background from './images/background.jpg'
 
 class App extends Component {
 
@@ -12,34 +13,11 @@ class App extends Component {
     }
   }
 
-  loginPage() {
-
-    document.body.style.backgroundImage = `url(${background})`
-    return (
-      <div>
-        <div className='loginDiv'>
-          <h2 className='adminHeading'>Admin Login</h2>
-          <div id='line'></div>
-          <div className="form-group">
-            <input type="email" className="form-control" placeholder="Enter email" />
-            <small id="emailHelp" className="form-text" style={{ color: 'white' }}>Email associated with admin account.</small>
-          </div>
-          <div className="form-group">
-            <input type="password" className="form-control" placeholder="Password" />
-          </div>
-          <div className='loginBtn'>
-            <button type="submit" className="btn btn-dark">Login</button>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
 
   render() {
     return (
       <div>
-        {this.loginPage()}
+        <Login />
       </div>
     );
   }
