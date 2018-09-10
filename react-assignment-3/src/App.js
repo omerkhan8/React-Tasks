77f084fa36b4ed11a1c027dd23e1dd9029f811ce
Omer Khan <omerkhan842@gmail.com>
2018-09-10 19:42:53 +0500

    edit and delete added

--- react-assignment-3/src/App.js
+++ react-assignment-3/src/App.js
@@ -16,8 +16,13 @@ class App extends Component {
       adForm: false,
       userData: [],
       index: null,
-      userObj: null
-
+      editObj: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        salary: '',
+        date: ''
+      }
     }
 
     this.loginUser = this.loginUser.bind(this);
@@ -25,28 +30,34 @@ class App extends Component {
     this.cancel = this.cancel.bind(this);
     this.submit = this.submit.bind(this);
     this.edit = this.edit.bind(this);
+    this.delete = this.delete.bind(this);
   }
 
   edit(index) {
     const { userData } = this.state;
-    let obj = {
-      firstName: userData[index].firstName,
-      lastName: userData[index].lastName,
-      email: userData[index].email,
-      salary: userData[index].salary,
-      date: userData[index].date
-    };
-    this.setState({ index, userObj: obj });
+    const { firstName, lastName, email, salary, date } = userData[index];
+    let editObj = { firstName, lastName, email, salary, date };
+    this.setState({ index, editObj });
   }
 
-  submit(obj) {
-    const { userData, index } = this.state;
-    index === null ? userData.push(obj) : userData[index] = obj;
+  delete(index) {
+    let { userData } = this.state;
+    userData.splice(index, 1);
     this.setState({ userData });
   }
 
+  submit(obj) {
+    let { userData, index, editObj } = this.state;
+    index === null ? userData.push(obj) : userData[index] = obj;
+    index = null;
+    editObj = { firstName: '', lastName: '', email: '', salary: '', date: '' };
+    this.setState({ userData, index, editObj });
+  }
+
   cancel() {
-    this.setState({ adForm: false })
+    let { editObj } = this.state;
+    editObj = { firstName: '', lastName: '', email: '', salary: '', date: '' };
+    this.setState({ adForm: false, editObj });
   }
 
   loginUser() {
@@ -63,8 +74,8 @@ class App extends Component {
     return (
       <div>
         {!user && <Login loginUser={this.loginUser} />}
-        {user && !adForm && <Table adToForm={this.adToForm} userData={this.state.userData} edit={this.edit} />}
-        {user && adForm && <Form cancel={this.cancel} submit={this.submit} index={this.state.index} userObj ={this.state.userObj} />}
+        {user && !adForm && <Table adToForm={this.adToForm} userData={this.state.userData} edit={this.edit} delete={this.delete} />}
+        {user && adForm && <Form cancel={this.cancel} submit={this.submit} index={this.state.index} editObj={this.state.editObj} />}
       </div>
     );
   }
