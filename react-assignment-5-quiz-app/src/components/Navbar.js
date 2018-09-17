8197ee27fb7e7fb74c0f88569efb7abbd16ae2f1
Omer Khan <omerkhan842@gmail.com>
2018-09-17 13:05:55 +0500

    navbar component

---
+++ react-assignment-5-quiz-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const Navbar = (props) => {
+    return (
+        <div className='nav'>
+            <h3 id='navHeading'>Quiz App</h3>
+        </div>
+    )
+}
+
+export default Navbar;
+
