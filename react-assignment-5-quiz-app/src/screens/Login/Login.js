15e45923f05dabbd228ab8306fa1b9e32e79a6c8
Omer Khan <omerkhan842@gmail.com>
2018-09-17 13:07:23 +0500

    login screen

--- react-assignment-5-quiz-app/src/screens/Login/Login.js
+++ react-assignment-5-quiz-app/src/screens/Login/Login.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './Login.css';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '../../components/Navbar';
 
-class Login extends Component{
-    render(){
-        return(
+class Login extends Component {
+    render() {
+        return (
             <div>
                 <Navbar />
+                <div className='loginDiv'>
+                    <h2 className='adminHeading'>Admin Login</h2>
+                    <div id='line'></div>
+                    <div className="form-group" style={{ marginTop: '30px' }}>
+                        <input type="email" className="form-control inputLogin" placeholder="Enter email" />
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control inputLogin" placeholder="Password" />
+                    </div>
+                    <div className='loginBtn'>
+                        <button type="submit" className="btn" >Login</button>
+                    </div>
+                </div>
+
             </div>
         )
     }
