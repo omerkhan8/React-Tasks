b1c5548856df34c8d087d164624138a93c522bcb
Omer Khan <omerkhan842@gmail.com>
2018-10-07 09:48:39 +0500

    posts component

---
+++ react-assignment-7-fb-posts/src/Components/Posts/Posts.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Posts.css';
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Posts;
