98e14d0ec12a5d8eb348e69f43f26a41f9e19ecf
Omer Khan <omerkhan842@gmail.com>
2018-10-07 09:48:56 +0500

    code updated

--- react-assignment-7-fb-posts/src/App.js
+++ react-assignment-7-fb-posts/src/App.js
@@ -1,11 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Posts from './Components/Posts/Posts';
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      postsObj: {
+        createdBy: 'Omer Khan',
+        avatar: 'link',
+        description: 'this is my first post',
+        images: [],
+        createdAt: Date.now(),
+        likes: []
+      }
+    }
+  }
+
+  render() {
+    return (
       <div>
-
+        <div className="postContainer">
+          <Posts />
+        </div>
       </div>
     )
   }
